026f63211135b47d336efe5416b80a21061f93d6
Sami Barghshoon <chugah@users.noreply.github.com>
2016-11-03 15:49:52 -0400

    Added content to example component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Examples extends Component {
 	render() {
 		return (
-			<h3>Examples Component!</h3>
+			<div>
+				<h3>Examples Component!</h3>
+				<p>Welcome to the example page.</p>
+			</div>
 		);
 	}
 }
