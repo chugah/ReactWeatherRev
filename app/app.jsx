e716b461e4137637892dbcefce98b959c560a3ed
Sami Barghshoon <chugah@users.noreply.github.com>
2016-11-07 12:47:52 -0500

    Add foundation & jquery to app

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 import Routes from 'Routes';
 
+// Load Foundaiton
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 ReactDOM.render(
 	<Router history={hashHistory} routes={Routes} />,
     document.getElementById('app')
